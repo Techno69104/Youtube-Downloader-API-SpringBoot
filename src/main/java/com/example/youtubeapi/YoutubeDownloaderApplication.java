aec7975f49cd20b98bf3ab21160206550d2841f1
Muzammil Shah <120221116+Techno69104@users.noreply.github.com>
2026-04-20 23:36:01 +0500

    YoutubeDownloaderApplication.java

---
+++ src/main/java/com/example/youtubeapi/YoutubeDownloaderApplication.java
@@ -0,0 +1,13 @@
+package com.example.youtubeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class YoutubeDownloaderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YoutubeDownloaderApplication.class, args);
+        System.out.println("YouTube Downloader API Started Successfully!");
+    }
+}
